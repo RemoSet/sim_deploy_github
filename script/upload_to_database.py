d89dd9471645bbda2d15aa333e380da29da00647
RemoSet <vogelrasse@mail.ru>
2025-05-25 17:06:13 +0300

    получилось для генерации, получится и here

--- script/upload_to_database.py
+++ script/upload_to_database.py
@@ -9,20 +9,24 @@ import pandas as pd
 
 
 config = configparser.ConfigParser()
-config.read('../config/config.ini')
-folder_data = eval(config['product']['folder_data'])
-folder_log = eval(config['logger']['folder_log'])
-os.makedirs(folder_log, exist_ok=True)
-file_log = os.path.join(folder_log, eval(config['logger']['upload_to_database_log']))
+dir_script = os.path.dirname(__file__)
+dir_project = os.path.dirname(dir_script)
+dir_config = os.path.join(dir_project, 'config')
+config.read(os.path.join(dir_config, 'config.ini'))
+dir_data = os.path.join(dir_project, eval(config['product']['folder_data']))
+dir_log = os.path.join(dir_project, eval(config['logger']['folder_log']))
+
+os.makedirs(dir_log, exist_ok=True)
+file_log = os.path.join(dir_log, eval(config['logger']['upload_to_database_log']))
 date_load = datetime.today().date()
 logging.basicConfig(level=logging.INFO, filename=file_log, filemode='w')
-load_file = glob.glob('*.csv', root_dir=folder_data)
+load_file = glob.glob('*.csv', root_dir=dir_data)
 if not load_file:
     logging.info(f'{date_load} : no data')
     sys.exit()
 df = pd.DataFrame()
 for file in load_file:
-    data = pd.read_csv(os.path.join(folder_data, file))
+    data = pd.read_csv(os.path.join(dir_data, file))
     df = pd.concat([df, data], ignore_index=True)
 connect = eval(config['database']['connect'])
 cursor = connect.cursor()
